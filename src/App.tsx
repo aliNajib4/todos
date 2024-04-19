a9b9599de44d23bfa8976d879cd9fc12b472cde6
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-04-19 02:41:00 +0200

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <p className="asd text-6xl text-red-500">hi!</p>
+    </>
+  )
+}
+
+export default App
+
+
+//* Todo items left All Active Completed Clear Completed Drag and drop to reorder list
