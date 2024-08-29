f2a44058987c51f3d96558d863b2301948036005
ali najib hegazy <alinajibhegazy4@gmail.com>
2024-08-29 09:49:30 +0300

    handle header buttons and sign out

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
 import DarkIcon from "../../../assets/images/icon-moon.svg?react";
 import LightIcon from "../../../assets/images/icon-sun.svg?react";
-import { useAppDispatch } from "../../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
 import { toggleTheme } from "../../../store/theme/themeSlice";
+import { actSignout } from "../../../store/auth/authSlice";
 
 type TProp = {
   theme: "light" | "dark";
@@ -10,9 +11,14 @@ type TProp = {
 
 const Header = ({ theme }: TProp) => {
   const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.auth.user);
 
   const changeTheme = () => {
     dispatch(toggleTheme());
+  };
+
+  const handleSignout = () => {
+    dispatch(actSignout());
   };
 
   return (
@@ -21,20 +27,32 @@ const Header = ({ theme }: TProp) => {
         todo
       </Link>
 
-      <div>
-        <Link
-          className="ml-3 select-none rounded-full border p-2 text-xl capitalize text-MLight hover:text-light"
-          to="/signin"
-        >
-          sign-in
-        </Link>
-        <Link
-          className="ml-3 select-none rounded-full border p-2 text-xl capitalize text-MLight hover:text-light"
-          to="/signup"
-        >
-          sign-up
-        </Link>
-      </div>
+      {user ? (
+        <>
+          <div className="text-MLight dark:text-mVD">{user.email}</div>{" "}
+          <button
+            onClick={handleSignout}
+            className="ml-3 select-none rounded-full border p-2 text-xl capitalize text-MLight hover:text-light"
+          >
+            signout
+          </button>
+        </>
+      ) : (
+        <div>
+          <Link
+            className="ml-3 select-none rounded-full border p-2 text-xl capitalize text-MLight hover:text-light"
+            to="/signin"
+          >
+            sign-in
+          </Link>
+          <Link
+            className="ml-3 select-none rounded-full border p-2 text-xl capitalize text-MLight hover:text-light"
+            to="/signup"
+          >
+            sign-up
+          </Link>
+        </div>
+      )}
 
       <button className="select-none" onClick={changeTheme}>
         {theme === "dark" ? <LightIcon /> : <DarkIcon />}
